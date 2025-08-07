3d47506de0ceee381759805513d64d0420c96a5f
Khosiyat <hosiyatsabirova@gmail.com>
2025-08-07 21:54:53 +0200

    permissions are added

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,1 +1,22 @@
 
+from rest_framework import permissions
+
+class IsParent(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.role == 'parent'
+
+class IsChild(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.role == 'child'
+
+class IsOwnerOrParent(permissions.BasePermission):
+    """
+    Allow child to view only their own data,
+    allow parent to access related child data.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.user.role == 'parent':
+            return obj.parents.filter(user=request.user).exists()
+        elif request.user.role == 'child':
+            return obj.user == request.user
+        return False
