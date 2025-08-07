419218476dc870402d0eb29ab6202faa3d4a2e1a
Khosiyat <hosiyatsabirova@gmail.com>
2025-08-07 21:57:48 +0200

    urls are added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,1 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ParentViewSet, ChildViewSet, ScoreTransactionViewSet, RewardViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
+router = DefaultRouter()
+router.register(r'parents', ParentViewSet, basename='parent')
+router.register(r'children', ChildViewSet, basename='child')
+router.register(r'score-transactions', ScoreTransactionViewSet, basename='scoretransaction')
+router.register(r'rewards', RewardViewSet, basename='reward')
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Login
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+]
