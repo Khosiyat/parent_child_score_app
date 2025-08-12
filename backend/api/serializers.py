ecf2607cfa19a39c26eb9b7a5dd59e8d69d6ee1d
Khosiyat <hosiyatsabirova@gmail.com>
2025-08-12 09:36:40 +0200

    serializers api is updated

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -51,3 +51,17 @@ class RewardSerializer(serializers.ModelSerializer):
         if request and hasattr(request, 'user') and request.user.role == 'parent':
             validated_data['parent'] = request.user.parent_profile
         return super().create(validated_data)
+
+
+
+from .models import ScoreTransaction, Reward
+
+class ScoreTransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScoreTransaction
+        fields = ['id', 'child', 'points', 'description', 'created_at']
+
+class RewardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reward
+        fields = ['id', 'name', 'cost', 'description']
